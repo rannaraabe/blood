66bd015e2a22611d06dd460ce29318f600ff0f3b
Leonandro <leonandrogurgel@gmail.com>
2022-01-25 19:09:16 -0300

    Adding endpoint

--- blood-app/lib/app/utils/backend_routes.dart
+++ blood-app/lib/app/utils/backend_routes.dart
@@ -6,6 +6,8 @@ class BackendRoutes {
   static const String LOGIN = BACKEND_DOMAIN + "/autenticacao";
 
   static const String USER = BACKEND_DOMAIN + "/usuario";
+  
+  static const String USER_BYUSERNAME = USER + "/byusername?username=";
 
   static const String DONATION_UNIT = BACKEND_DOMAIN + "/unidadedoacao";
 
