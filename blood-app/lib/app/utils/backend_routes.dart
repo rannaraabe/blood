db373882d59719a166c335ba67d77bb9043d691f
Leonandro <leonandrogurgel@gmail.com>
2022-01-25 13:52:47 -0300

    Adding initial routes to backend

---
+++ blood-app/lib/app/utils/backend_routes.dart
@@ -0,0 +1,13 @@
+class BackendRoutes {
+  BackendRoutes._();
+
+  static const String BACKEND_DOMAIN = "https://blood-backend1.herokuapp.com";
+
+  static const String LOGIN = BACKEND_DOMAIN + "/autenticacao";
+
+  static const String USER = BACKEND_DOMAIN + "/usuario";
+
+  static const String DONATION_UNIT = BACKEND_DOMAIN + "/unidadedoacao";
+
+  static const String PUBLICATION = BACKEND_DOMAIN + "/publicacao";
+}
