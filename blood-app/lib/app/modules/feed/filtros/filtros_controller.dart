07c5e7d41ee629d646f2fce276ca9819e5046daf
camiladuartes_ <camiladuartes_@ufrn.edu.br>
2022-02-07 11:09:24 -0300

    feat: filtro and favoritos gesture detectors

---
+++ blood-app/lib/app/modules/feed/filtros/filtros_controller.dart
@@ -0,0 +1,10 @@
+import 'package:mobx/mobx.dart';
+part 'filtros_controller.g.dart';
+
+class FiltrosController = _FiltrosControllerBase with _$FiltrosController;
+
+abstract class _FiltrosControllerBase with Store {
+  
+  _FiltrosControllerBase();
+  
+}
---
+++ blood-app/lib/app/modules/feed/widgets/side_gradient.dart
@@ -0,0 +1,27 @@
+import 'package:blood_app/app/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class SideGradient extends StatelessWidget {
+  const SideGradient({ Key? key, }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Container(
+      width: width * 0.04,
+      height: height * 0.04,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        gradient: AppTheme.sideGradient,
+        boxShadow: const [
+          BoxShadow(
+            color: AppTheme.pink,
+            spreadRadius: 30,
+            blurRadius: 12,
+          ),
+        ],
+      ),
+    );
+  }
+}
